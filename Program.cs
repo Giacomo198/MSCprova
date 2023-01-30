e33bbd307c7f285cc9199eaea33d7bbcbd6010ec
Giacomo Campisi (External) <e-giacomo.campisi@msc.com>
2023-01-30 12:24:29 +0100

    Prova per il restore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Prima Modifica Prova");
+Console.WriteLine("Hello, Prima Modifica Prova aaaaaaaaaaaaaaaaaaaaaaa MSC");
