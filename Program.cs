4c4abd50d2de8f94be5656fc413f8605a42647b6
Giacomo Campisi <e-giacomo.campisi@msc.com>
2023-01-30 15:56:24 +0100

    modifica per github

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Prima Modifica Prova aaaaaaaaaaaaaaaaaaaaaaa MSC");
+Console.WriteLine("Hello, Seconda modifica");
